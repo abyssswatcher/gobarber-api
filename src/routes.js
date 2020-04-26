2dfa926530821abdcc22284e61d8f3607875a4a0
hugogomess <hugogomes02@gmail.com>
2020-04-26 13:31:55 -0300

    create user controller and add store method

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ message: 'api home!' });
+  const routesSummary = [
+    '/users'
+  ]
+
+  return res.json({ routes: routesSummary });
 });
+
+// Users routes
+routes.post('/users', UserController.store);
 
 export default routes;
