9baa0189f2ef9685baaef8827afce966bd6d3077
hugogomess <hugogomes02@gmail.com>
2020-05-06 05:57:22 -0300

    add store into file controller

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
@@ -7,6 +9,7 @@ import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.get('/', (req, res) => {
   const routesSummary = ['/users'];
@@ -25,6 +28,6 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 // Uploads
-routes.post('/files', FileController.store);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
