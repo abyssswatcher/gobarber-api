fbb49b69646097fb542c96cd25956e7c969fdd35
hugogomess <hugogomes02@gmail.com>
2020-05-06 11:06:09 -0300

    create appointment controller

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -33,5 +34,8 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 // Providers
 routes.get('/providers', ProviderController.index);
+
+// Appointment
+routes.post('/appointments', AppointmentController.store);
 
 export default routes;
