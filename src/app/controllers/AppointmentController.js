964ec6918d162c91c899e349e2d41007a8477fed
hugogomess <hugogomes02@gmail.com>
2020-05-06 11:35:18 -0300

    update appointment controller

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import Appointment from '../models/Appointment';
+import User from '../models/User';
 
 class AppointmentController {
   async store(req, res) {
@@ -9,10 +10,27 @@ class AppointmentController {
     });
 
     if (!(await schema.isValid(req.body))) {
-      return res.json({ error: 'Validation fails.' });
+      return res.status(400).json({ error: 'Validation fails.' });
     }
 
-    return res.json({ ok: true });
+    const { provider_id, date } = req.body;
+    const isProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!isProvider) {
+      return res.status(400).json({
+        error: 'You only can create an appointment with a provider user.',
+      });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointment);
   }
 }
 
