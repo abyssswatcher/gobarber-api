66d223a971a62fdaa795f59a330597af21e14720
hugogomess <hugogomes02@gmail.com>
2020-05-06 12:27:48 -0300

    create schedule controller

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -38,5 +39,8 @@ routes.get('/providers', ProviderController.index);
 // Appointment
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+
+// Schedule
+routes.get('/schedule', ScheduleController.index);
 
 export default routes;
