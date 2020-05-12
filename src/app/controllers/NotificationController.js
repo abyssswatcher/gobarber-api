eda11ae9e19c2d8a460e1b973ff59df15390312c
hugogomess <hugogomes02@gmail.com>
2020-05-12 11:54:50 -0300

    add business logic into notification controller

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -1,8 +1,25 @@
 import Notification from '../schemas/Notification';
+import User from '../models/User';
 
 class NotificationController {
   async index(req, res) {
-    return res.json({ ok: true });
+    const isProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!isProvider) {
+      return res.status(401).json({
+        error: 'Only providers can see notifications.',
+      });
+    }
+
+    const notifications = await Notification.find({
+      user: req.userId,
+    })
+      .sort({ createdAt: -1 })
+      .limit(20);
+
+    return res.json(notifications);
   }
 }
 
