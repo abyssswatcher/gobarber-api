fbc5334b0c00bed590856cdd43d5e76951d58263
hugogomess <hugogomes02@gmail.com>
2020-05-13 16:25:51 -0300

    config smtp

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '50c8404f2fbd03',
+    pass: '2d89395fe8caf0',
+  },
+  default: {
+    from: 'GoBarber <noreply@gobarber.com',
+  },
+};
