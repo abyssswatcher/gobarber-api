d84225167d45a5f3664f963a26523a37ea402820
hugogomess <hugogomes02@gmail.com>
2020-05-18 06:45:41 -0300

    add past virtual field into appointment

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -47,7 +47,7 @@ class AvailableController {
       '19:00',
     ];
 
-    const available = schedule.map(time => {
+    const available = schedule.map((time) => {
       const [hour, minutes] = time.split(':');
       const value = setSeconds(
         setMinutes(setHours(searchDate, hour), minutes),
