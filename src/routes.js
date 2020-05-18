4a000057e94e1aff44beed9b65f09d6ab020dad4
hugogomess <hugogomes02@gmail.com>
2020-05-18 04:07:37 -0300

    set available route

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -36,7 +37,7 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 // Providers
 routes.get('/providers', ProviderController.index);
-routes.get('/providers/:providerId/available', );
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 // Appointment
 routes.post('/appointments', AppointmentController.store);
