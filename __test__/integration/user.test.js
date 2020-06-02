bf996745d06128c18f365c94ff6ccadda066d18d
hugogomess <hugogomes02@gmail.com>
2020-06-02 13:01:20 -0300

    add email test

--- __test__/integration/user.test.js
+++ __test__/integration/user.test.js
@@ -11,4 +11,23 @@ describe('User', () => {
 
     expect(response.body).toHaveProperty('id');
   });
+
+  it('should return email already exists error', async () => {
+    await request(app).post('/users').send({
+      name: 'Test User',
+      email: 'test@test.test',
+      password: 'passwordStrong!@#$ pindamonhagaba',
+    });
+
+    const response = await request(app).post('/users').send({
+      name: 'Test User 2',
+      email: 'test@test.test',
+      password: 'passwordStrong!@#$ pindamonhagaba',
+    });
+
+    expect(response.body).toHaveProperty('error');
+    expect(response.body.error).toEqual(
+      'Email already exists, try another one.'
+    );
+  });
 });
