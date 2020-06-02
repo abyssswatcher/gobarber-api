c0e591c74823e5fb84790c1ecef559d706ab1a01
hugogomess <hugogomes02@gmail.com>
2020-06-02 13:54:57 -0300

    update user tets with factory

--- __test__/integration/user.test.js
+++ __test__/integration/user.test.js
@@ -1,6 +1,7 @@
 import request from 'supertest';
 
 import app from '../../src/app';
+import factory from '../factories';
 import truncate from '../util/truncate';
 import User from '../../src/app/models/User';
 
@@ -10,44 +11,31 @@ describe('User', () => {
   });
 
   it('should be able to register', async () => {
-    const response = await request(app).post('/users').send({
-      name: 'Test User',
-      email: 'test@test.test',
-      password: 'passwordStrong!@#$ pindamonhagaba',
-    });
+    const user = await factory.attrs('User');
+
+    const response = await request(app).post('/users').send(user);
 
     expect(response.body).toHaveProperty('id');
   });
 
   it('should not be able to register with duplicated email', async () => {
-    await request(app).post('/users').send({
-      name: 'Test User',
-      email: 'test@test.test',
-      password: 'passwordStrong!@#$ pindamonhagaba',
-    });
+    const user = await factory.create();
 
-    const response = await request(app).post('/users').send({
-      name: 'Test User 2',
-      email: 'test@test.test',
-      password: 'passwordStrong!@#$ pindamonhagaba',
-    });
+    const response = await request(app).post('/users').send(user);
 
-    expect(response.body).toHaveProperty('error');
     expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('error');
     expect(response.body.error).toEqual(
       'Email already exists, try another one.'
     );
   });
 
   it('should encrypt user password when new user is created', async () => {
-    const response = await request(app).post('/users').send({
-      name: 'Test User',
-      email: 'test@test.test',
-      password: 'passwordStrong!@#$ pindamonhagaba',
+    const user = await factory.create({
+      password: 'strongPassword!@#$ pindamonhagaba',
     });
 
-    const user = await User.findByPk(response.id);
-    const isEncrypted = user.compareHash('passwordStrong!@#$ pindamonhagaba');
+    const isEncrypted = user.compareHash('strongPassword!@#$ pindamonhagaba');
 
     expect(isEncrypted).toBeTruthy();
   });
